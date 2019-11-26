764429407cda2553f4cbf65cf20b7ac7cc7f799a
Noah Snelson <noah.snelson@protonmail.com>
2019-11-25 20:12:38 -0800

    Added stack implementation

---
+++ lists/Stack.cpp
@@ -0,0 +1,63 @@
+
+#include<iostream>
+using std::cout;
+using std::endl;
+#include<string>
+using std::string;
+
+const size_t DEFAULT_CAP=5;
+
+template<typename T>
+class Stack{
+    public:
+        Stack(){
+            data = new T[DEFAULT_CAP];
+        }
+        ~Stack(){
+            delete[] data;
+        }
+        void push(const T& val){ //push onto stack
+            if(length == cap){
+                cout << "Reached capacity " << cap << ", allocating new array" << endl;
+                cap = cap*2;
+                T* newData = new T[cap];
+                for(size_t i=0; i<length; i++){
+                    newData[i] = data[i];
+                }
+                delete[] data;
+                data = newData;
+            }
+            data[length] = val;
+            length += 1;
+        }
+        T top() const{
+            return data[length];
+        }
+        void pop(){
+            auto ret = data[length];
+            length -= 1;
+        }
+        void print(){
+            for(size_t i=0; i<length; i++){
+                cout << data[i] << " ";
+            }
+            cout << endl;
+        }
+
+    private:
+        T* data;
+        size_t head;
+        size_t tail;
+        size_t cap=DEFAULT_CAP;
+        size_t length = 0;
+};
+
+int main(){
+    Stack<int> s;
+    for(int i = 0; i < DEFAULT_CAP*4; i++){
+        s.push(i);
+    }
+    s.print();
+    s.pop();
+    s.print();
+}
